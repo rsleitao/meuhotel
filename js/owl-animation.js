6a69cb09c603f24fd29f34aec03717d2cad78671
rsleitao <renato_sleitao@hotmail.com>
2024-03-06 15:03:36 +0000

    06_03_v0.6

--- js/owl-animation.js
+++ js/owl-animation.js
@@ -7,9 +7,3 @@ owl.owlCarousel({
     autoplayTimeout:5000,
     autoplayHoverPause:false
 });
-$('.play').on('click',function(){
-    owl.trigger('play.owl.autoplay',[1000])
-})
-$('.stop').on('click',function(){
-    owl.trigger('stop.owl.autoplay')
-})
